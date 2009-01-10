b4b04d5ecc41a5ba2bdae06c6c7aa06da57eee7f
Jeremy Wells <jeremy@boost.co.nz>
2009-01-10 16:05:37 +1300

    Added test widget for svn task

--- app/helpers/task_types_helper.rb
+++ app/helpers/task_types_helper.rb
@@ -1,2 +1,12 @@
 module TaskTypesHelper
+  def task_widget(task)
+    @task_type = task.task_type_class
+    @attributes = @task_type.attributes
+
+    begin
+      render :file => 'task_types/%s_widget.html' % @task_type.name.underscore
+    rescue ActionView::MissingTemplate
+      render :text => 'No widget?'
+    end
+  end
 end
